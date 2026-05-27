00fc23fb4eb78696908730c71681a8ca90ce1f34
CountermaxNoe <161792011+CountermaxNoe@users.noreply.github.com>
2026-05-27 13:34:32 -0600

    Creacion de ViewModel doble

---
+++ Areas/Admin/ViewModels/AdminPanelViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HackaTec.Areas.Admin.ViewModels
+{
+    public class AdminPanelViewModel
+    {
+        public IndexAgregarViewModel? FormularioInstitucion { get; set; }
+        public List<IndexViewModel>? ListaInstituciones { get; set; }
+    }
+}
