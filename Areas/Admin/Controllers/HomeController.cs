8225af5e4c8f2dc2e2337d758d8c4f13704decc4
CountermaxNoe <161792011+CountermaxNoe@users.noreply.github.com>
2026-05-27 13:02:37 -0600

    Comentarios service de admin

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ namespace HackaTec.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         [Area("Admin")]
         public IActionResult Index()
         {
--- Services/AdminService.cs
+++ Services/AdminService.cs
@@ -18,5 +18,11 @@ namespace HackaTec.Services
 
             return admin;
         }
+
+        //Servicios Requeridos
+        // Agregar una escuela con sus datos(los datos son los de la clase IndexAgregarViewModel)
+        // La escuela no debe de existir previamente, se debe validar que no exista una escuela con el mismo nombre o dirección.
+        // Traer los datos de las escuelas para mostrar en las vistas(los daotos estan en la IndexViewModel)
+        // Inabiltar o habilitar una escuela, esto se hace cambiando el estado de la escuela, si el estado es false la escuela esta inabilitada y si el estado es true la escuela esta habilitada.
     }
 }
