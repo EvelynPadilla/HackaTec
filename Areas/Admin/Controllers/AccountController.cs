82db0a3935f65cededfac4c87a90757c0fca6b1d
CountermaxNoe <161792011+CountermaxNoe@users.noreply.github.com>
2026-05-27 11:18:10 -0600

    Repositorio Generico, Service Admin(login), Helper, AccountController

---
+++ Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+﻿using HackaTec.Areas.Admin.ViewModels;
+using HackaTec.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace HackaTec.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : Controller
+    {
+        private readonly AdminService adminService;
+
+        public AccountController(AdminService adminService)
+        {
+            this.adminService = adminService;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginAdminVioewModel model)
+        {
+            var admin = adminService.Login(model);
+
+            if (admin == null)
+            {
+                ModelState.AddModelError("", "Contraseña o Nickname incorrectos");
+                return View(model);
+            }
+            else if (admin.Usuario != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier,
+                    admin.Id.ToString()),
+                    new Claim(ClaimTypes.Name,
+                        admin.Usuario)
+                };
+
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(principal);
+
+                return RedirectToAction("Index", "Home", new { area = "Admin" });
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using HackaTec.Models.Entities;
+
+namespace HackaTec.Repositories
+{
+
+    public class Repository<T> where T : class
+    {
+        public Repository(HackatecContext context)
+        {
+            Context = context;
+        }
+        public HackatecContext Context { get; }
+        public virtual T? Get(object id)
+        {
+            return
+                Context.Find<T>(id);
+        }
+        public virtual IEnumerable<T> GetAll()
+        {
+            return Context.Set<T>();
+        }
+        public virtual void Insert(T entity)
+        {
+            Context.Add(entity);
+            Context.SaveChanges();
+        }
+        public virtual void Update(T entity)
+        {
+            Context.Update(entity);
+            Context.SaveChanges();
+        }
+
+
+        public virtual void Delete(object id)
+        {
+            var entity = Context.Find<T>(id);
+            if (entity != null)
+            {
+                Context.Remove(entity);
+                Context.SaveChanges();
+            }
+        }
+    }
+}
