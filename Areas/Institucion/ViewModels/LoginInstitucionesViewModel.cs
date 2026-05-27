b197444bbc01f525a1b3c9a5a548116ceb9c23c1
CountermaxNoe <161792011+CountermaxNoe@users.noreply.github.com>
2026-05-27 15:44:57 -0600

    n

--- Areas/Institucion/ViewModels/LoginInstitucionesViewModel.cs
+++ Areas/Institucion/ViewModels/LoginInstitucionesViewModel.cs
@@ -4,15 +4,15 @@ namespace HackaTec.Areas.Institucion.ViewModels
 {
     public class LoginInstitucionesViewModel
     {
-        [Required(ErrorMessage ="El CCT/Correo es obligatorio")]
-        [RegularExpression(@"^([a-zA-Z0-9]{10}|[^@\s]+@[^@\s]+\.[^@\s]+)$", ErrorMessage ="Ingrese un CCT de 10 caracteres o un correo electronico valido")]
+        [Required(ErrorMessage = "El CCT/Correo es obligatorio")]
+        [RegularExpression(@"^([a-zA-Z0-9]{10}|[^@\s]+@[^@\s]+\.[^@\s]+)$", ErrorMessage = "Ingrese un CCT de 10 caracteres o un correo electronico valido")]
         [DataType(DataType.Text)]
-        public string Correo_CCT { get; set; }
+        public string Correo_CCT { get; set; } = null!;
 
         [Required(ErrorMessage = "La contraseña es obligatoria.")]
         [StringLength(100, ErrorMessage = "La contraseña no puede exceder los 100 caracteres")]
         [DataType(DataType.Password)]
-        public string Contrasena { get; set; }
+        public string Contrasena { get; set; } = null!;
 
     }
 }
