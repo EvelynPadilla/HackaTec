3817f2c08b3667eae1d0456c865c1a01cfcfff7c
CountermaxNoe <161792011+CountermaxNoe@users.noreply.github.com>
2026-05-27 11:45:04 -0600

    Ya abre el inicio de sesion y cambie las vistas de carpeta junto a el nomnre

--- Program.cs
+++ Program.cs
@@ -1,6 +1,38 @@
+using HackaTec.Models.Entities;
+using HackaTec.Repositories;
+using HackaTec.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Admin/Account/Login";
+        options.AccessDeniedPath = "/Admin/Account/Login";
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddDbContext<HackatecContext>();
+
+builder.Services.AddScoped(typeof(Repository<>), typeof(Repository<>));
+builder.Services.AddScoped<AdminService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+);
+
+app.MapDefaultControllerRoute();
 
 app.Run();
