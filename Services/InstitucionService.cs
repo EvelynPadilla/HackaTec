3e9f21069d134f576e0a6786afcc28596dd82549
Jared <231g0168@rcarbonifera.tecnm.mx>
2026-05-27 15:45:41 -0600

    Cambio del script de base de datos y avance en InstitucionesServices

--- Services/InstitucionService.cs
+++ Services/InstitucionService.cs
@@ -1,10 +1,37 @@
-﻿namespace HackaTec.Services
+﻿using HackaTec.Areas.Admin.ViewModels;
+using HackaTec.Areas.Institucion.ViewModels;
+using HackaTec.Models.Entities;
+using HackaTec.Repositories;
+
+
+namespace HackaTec.Services
 {
     public class InstitucionService
     {
-        public InstitucionService() 
+        private readonly Repository<InstitucionesEducativas> repoEscuelas;
+        private readonly Repository<PublicacionesNecesidad> repoPublicaciones;
+        public InstitucionService(Repository<InstitucionesEducativas> repoEscuelas, Repository<PublicacionesNecesidad> repoPublicaciones)
         {
-
+            this.repoEscuelas = repoEscuelas;
+            this.repoPublicaciones = repoPublicaciones;
         }
+        //public InstitucionesEducativas? Login(LoginInstitucionesViewModel model)
+        //{
+        //    bool ingresoCorrreo = model.Correo_CCT.Contains("@");
+            
+        //    if (ingresoCorrreo)
+        //    {
+        //        var institucion = repoEscuelas.GetAll().FirstOrDefault(i =>
+        //        i. == model.Correo_CCT);
+        //        return institucion;
+        //    }
+        //    else
+        //    {
+        //        var institucion = repoEscuelas.GetAll().FirstOrDefault(i =>
+        //        i.Cct == model.Correo_CCT);
+        //        return institucion;
+        //    }
+           
+        //}
     }
 }
--- wwwroot/hackatec-script.sql
+++ wwwroot/hackatec-script.sql
@@ -1,190 +1,97 @@
-CREATE DATABASE  IF NOT EXISTS `hackatec` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `hackatec`;
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
---
--- Host: localhost    Database: hackatec
--- ------------------------------------------------------
--- Server version	8.0.36
+CREATE DATABASE Hackatec;
+USE Hackatec;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE instituciones_educativas (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    cct VARCHAR(10) UNIQUE NOT NULL,
+    nombre VARCHAR(200) NOT NULL,
+    contrasena varchar(255) not null,
+    direccion VARCHAR(255) NOT NULL,
+    persona_responsable VARCHAR(100) NOT NULL, 
+    telefono_escuela VARCHAR(10) NOT NULL,
+    telefono_responsable VARCHAR(10) NOT NULL,
+    estado BOOL NOT NULL DEFAULT TRUE
+);
 
---
--- Table structure for table `administrador`
---
+CREATE TABLE usuario (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    apellidos VARCHAR(100) NULL,
+    correo VARCHAR(150) NOT NULL UNIQUE,
+    contrasena_hash VARCHAR(255) NOT NULL,
+    telefono VARCHAR(10) NOT NULL,
+    rol VARCHAR(50) NOT NULL DEFAULT 'Persona',
+    foto_perfil VARCHAR(255) NULL
+);
 
-DROP TABLE IF EXISTS `administrador`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `administrador` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `Usuario` varchar(30) NOT NULL,
-  `Contraseña` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE publicaciones_necesidad (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_institucion INT NOT NULL,
+    titulo VARCHAR(150) NOT NULL,
+    descripcion TEXT NOT NULL,
+    fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
+    estado BOOL NOT NULL DEFAULT TRUE, -- TRUE = Activo, FALSE = Cubierto
+    CONSTRAINT FK_institucion_publicaciones FOREIGN KEY (id_institucion) REFERENCES instituciones_educativas(id)
+);
 
---
--- Table structure for table `imagenes_necesidad`
---
 
-DROP TABLE IF EXISTS `imagenes_necesidad`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `imagenes_necesidad` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_publicacion` int NOT NULL,
-  `ruta_imagen` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_Imagen_Necesidad` (`id_publicacion`),
-  CONSTRAINT `FK_Imagen_Necesidad` FOREIGN KEY (`id_publicacion`) REFERENCES `publicaciones_necesidad` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE imagenes_necesidad (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_publicacion INT NOT NULL,
+    ruta_imagen VARCHAR(255) NOT NULL,
+    CONSTRAINT FK_Imagen_Necesidad FOREIGN KEY (id_publicacion) REFERENCES publicaciones_necesidad(id) ON DELETE CASCADE
+);
 
---
--- Table structure for table `instituciones_educativas`
---
+CREATE TABLE publicaciones_agradecimiento (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_publicacion_necesidad INT NOT NULL,
+    id_usuario_donante INT NOT NULL,
+    descripcion TEXT NOT NULL,
+    ruta_fotografia VARCHAR(255) NULL,
+    fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT FK_Necesidad_Agradecimiento FOREIGN KEY (id_publicacion_necesidad) REFERENCES publicaciones_necesidad(id),
+    CONSTRAINT FK_Usuario_Agradecimiento FOREIGN KEY (id_usuario_donante) REFERENCES usuario(id)
+);
 
-DROP TABLE IF EXISTS `instituciones_educativas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `instituciones_educativas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `cct` varchar(10) NOT NULL,
-  `nombre` varchar(200) NOT NULL,
-  `direccion` varchar(255) NOT NULL,
-  `persona_responsable` varchar(100) NOT NULL,
-  `telefono_escuela` varchar(10) NOT NULL,
-  `telefono_responsable` varchar(10) NOT NULL,
-  `estado` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `cct` (`cct`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE salas_chat (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_usuario_donante INT NOT NULL,
+    id_institucion INT NOT NULL,
+    id_post_necesidad INT NULL, -- NULL si es oferta libre, ID si es "Quiero Donar"
+    fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT FK_Chat_Donante FOREIGN KEY (id_usuario_donante) REFERENCES usuario(id),
+    CONSTRAINT FK_Chat_Institucion FOREIGN KEY (id_institucion) REFERENCES instituciones_educativas(id),
+    CONSTRAINT FK_Chat_Post FOREIGN KEY (id_post_necesidad) REFERENCES publicaciones_necesidad(id)
+);
 
---
--- Table structure for table `mensajes`
---
+CREATE TABLE mensajes (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_sala INT NOT NULL,
+    remitente_tipo ENUM('Donante', 'Escuela') NOT NULL,
+    id_remitente INT NOT NULL,
+    contenido TEXT NULL,
+    ruta_imagen VARCHAR(255) NULL,
+    estado_leido BOOL NOT NULL DEFAULT FALSE, -- FALSE = no_leido
+    fecha_envio DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT FK_Mensaje_Sala FOREIGN KEY (id_sala) REFERENCES salas_chat(id)
+);
 
-DROP TABLE IF EXISTS `mensajes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `mensajes` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_sala` int NOT NULL,
-  `remitente_tipo` enum('Donante','Escuela') NOT NULL,
-  `id_remitente` int NOT NULL,
-  `contenido` text,
-  `ruta_imagen` varchar(255) DEFAULT NULL,
-  `estado_leido` tinyint(1) NOT NULL DEFAULT '0',
-  `fecha_envio` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `FK_Mensaje_Sala` (`id_sala`),
-  CONSTRAINT `FK_Mensaje_Sala` FOREIGN KEY (`id_sala`) REFERENCES `salas_chat` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Administrador:
 
---
--- Table structure for table `publicaciones_agradecimiento`
---
+create table Administrador(
+id int not null primary key auto_increment,
+nombre varchar(80) not null,
+contrasena varchar(255) not null
+);
 
-DROP TABLE IF EXISTS `publicaciones_agradecimiento`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `publicaciones_agradecimiento` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_publicacion_necesidad` int NOT NULL,
-  `id_usuario_donante` int NOT NULL,
-  `descripcion` text NOT NULL,
-  `ruta_fotografia` varchar(255) DEFAULT NULL,
-  `fecha` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `FK_Necesidad_Agradecimiento` (`id_publicacion_necesidad`),
-  KEY `FK_Usuario_Agradecimiento` (`id_usuario_donante`),
-  CONSTRAINT `FK_Necesidad_Agradecimiento` FOREIGN KEY (`id_publicacion_necesidad`) REFERENCES `publicaciones_necesidad` (`id`),
-  CONSTRAINT `FK_Usuario_Agradecimiento` FOREIGN KEY (`id_usuario_donante`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `publicaciones_necesidad`
---
 
-DROP TABLE IF EXISTS `publicaciones_necesidad`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `publicaciones_necesidad` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_institucion` int NOT NULL,
-  `titulo` varchar(150) NOT NULL,
-  `descripcion` text NOT NULL,
-  `fecha` datetime DEFAULT CURRENT_TIMESTAMP,
-  `estado` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `FK_institucion_publicaciones` (`id_institucion`),
-  CONSTRAINT `FK_institucion_publicaciones` FOREIGN KEY (`id_institucion`) REFERENCES `instituciones_educativas` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Trigger Institucion
+DELIMITER //
+DROP TRIGGER IF EXISTS `hackatec`.`instituciones_educativas_AFTER_INSERT`//
 
---
--- Table structure for table `salas_chat`
---
-
-DROP TABLE IF EXISTS `salas_chat`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `salas_chat` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_usuario_donante` int NOT NULL,
-  `id_institucion` int NOT NULL,
-  `id_post_necesidad` int DEFAULT NULL,
-  `fecha_creacion` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `FK_Chat_Donante` (`id_usuario_donante`),
-  KEY `FK_Chat_Institucion` (`id_institucion`),
-  KEY `FK_Chat_Post` (`id_post_necesidad`),
-  CONSTRAINT `FK_Chat_Donante` FOREIGN KEY (`id_usuario_donante`) REFERENCES `usuario` (`id`),
-  CONSTRAINT `FK_Chat_Institucion` FOREIGN KEY (`id_institucion`) REFERENCES `instituciones_educativas` (`id`),
-  CONSTRAINT `FK_Chat_Post` FOREIGN KEY (`id_post_necesidad`) REFERENCES `publicaciones_necesidad` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `usuario`
---
-
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuario` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `apellidos` varchar(100) NOT NULL,
-  `correo` varchar(150) NOT NULL,
-  `contrasena_hash` varchar(255) NOT NULL,
-  `telefono` varchar(10) NOT NULL,
-  `rol` varchar(50) NOT NULL DEFAULT 'Persona',
-  `foto_perfil` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `correo` (`correo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2026-05-27 11:13:12
+CREATE DEFINER = CURRENT_USER TRIGGER `hackatec`.`instituciones_educativas_AFTER_INSERT` AFTER INSERT ON `instituciones_educativas` FOR EACH ROW
+BEGIN
+	insert into usuarios(nombre, correo, contrasena_hash, telefono, rol) values(new.nombre, new.cct, new.contrasena, new.telefono_escuela, "Institución");
+END
+DELIMITER ;
