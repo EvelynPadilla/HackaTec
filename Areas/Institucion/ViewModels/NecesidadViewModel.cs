6a09c9ccafb0c90cae97c84a7b99506cdd348a90
CountermaxNoe <161792011+CountermaxNoe@users.noreply.github.com>
2026-05-27 18:29:57 -0600

    h

---
+++ Areas/Institucion/ViewModels/NecesidadViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HackaTec.Areas.Institucion.ViewModels
+{
+    public class NecesidadViewModel
+    {
+        public string Material { get; set; } = null!;
+        public string Uso { get; set; } = null!;
+        public string Imagen { get; set; } = null!;
+    }
+}
